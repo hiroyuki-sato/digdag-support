0dd8ecb3f2ee690373f905628c9ebd84ad6530e1
Hiroyuki Sato <hiroysato@gmail.com>
2018-11-30 15:37:30 +0900

    Add core 925

---
+++ core_925_python_secrets/python_args.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import digdag
+import os
+
+def required_arguments(required1, required2):
+    print("required1 = " + str(required1))
+    print("required2 = " + str(required2))
+
+def optional_arguments(optional1=None, optional2="default"):
+    print("optional1 = " + str(optional1))
+    print("optional2 = " + str(optional2))
+
+def mixed_arguments(arg1, arg2=None, arg3=None):
+    print("arg1 = " + str(arg1))
+    print("arg2 = " + str(arg2))
+    print("arg3 = " + str(arg3))
+
+def keyword_arguments(arg1, **kw):
+    print("arg1 = " + str(arg1))
+    print("keywords = " + str(kw))
+
+def env_argument():
+    print("arg1 = " + str(os.environ.get('required1')))
+
